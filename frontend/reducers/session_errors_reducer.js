a2f6a748a4d86f44362c9cd5fd0385529526de5c
Michael Ruan <michaelruan1@gmail.com>
2018-03-07 14:53:50 -0500

    fix errors

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -6,7 +6,7 @@ const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch(action.type){
     case RECEIVE_SESSION_ERRORS:
-      return merge([], state, action.errors);
+      return action.errors;
     case RECEIVE_CURRENT_USER:
       return [];
     default:
