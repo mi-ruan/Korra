d977fb7b4ded6240178079db228ea7490870847e
Michael Ruan <michaelruan1@gmail.com>
2018-03-16 08:57:20 -0400

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-users = User.create([{
-  name: Faker::Name.unique.name,
-  email: Faker::Internet.email,
-  password: 'starwars',
-  }])
