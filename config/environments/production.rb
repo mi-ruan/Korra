f635a88f5bedd8bf7f8a89c058e42fbba824a287
Michael Ruan <michaelruan1@gmail.com>
2018-07-09 20:23:14 -0400

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
   config.assets.compress = true
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
